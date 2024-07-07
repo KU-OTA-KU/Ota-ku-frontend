d80d49fc2ddb5a4868dfc4fa03ced121fd93284b
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-07-07 20:36:47 +0400

    add changes and fix bugs

---
+++ dist/_nuxt/JrQqGp-a.js
@@ -0,0 +1,1 @@
+import{d as e,_ as n}from"./1uB6RpWs.js";const t=e({name:"CatalogView"});function o(s,a,r,c,_,p){return null}const f=n(t,[["render",o]]);export{f as default};
---
+++ dist/_nuxt/p29KBVLb.js
@@ -0,0 +1,1 @@
+import{d as e,_ as n}from"./1uB6RpWs.js";const t=e({name:"CatalogView"});function o(s,a,r,c,_,p){return null}const d=n(t,[["render",o]]);export{d as default};
