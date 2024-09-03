a84a8d4730cebe1907691f3a0f03d8b03d7125ea
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-09-03 17:34:12 +0400

    changes

--- android/app/src/main/java/com/otaku/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.otaku.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- app.vue
+++ app.vue
@@ -1,27 +1,6 @@
 <template>
-  <NuxtLayout />
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { App as CapacitorApp } from '@capacitor/app';
-
-export default defineComponent({
-  name: "appGlobalComponent",
-
-  mounted() {
-    let lastBackPress = 0;
-    CapacitorApp.addListener('backButton', () => {
-      const currentTime = new Date().getTime();
-      if (currentTime - lastBackPress < 1500) {
-        CapacitorApp.exitApp();
-      } else {
-        lastBackPress = currentTime;
-        window.history.back();
-      }
-    });
-  },
-});
-</script>
-
-<style>
-</style>
