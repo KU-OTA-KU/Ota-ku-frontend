f1ccb0e21967bec2462d360bd46a2472efb64c11
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-09-09 12:40:46 +0400

    fix nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     ssr: true,
     devtools: {enabled: true},
+    app: {
+        baseURL: '/Ota-ku-frontend/'
+    },
     modules: [
       '@nuxtjs/tailwindcss',
       '@nuxtjs/google-fonts',
