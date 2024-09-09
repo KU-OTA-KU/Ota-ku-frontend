5a9516c643ea2da86ee1b27aaa6a1e65f1c72315
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-09-09 14:33:24 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
                 'big': 'calc(1920px - (162px * 2px))',
                 'medium': 'calc(1280px - (162px * 2px))',
                 'small': 'calc(1920px - (162px * 2px))',
+                'small2': 'calc(1920px - (162px * 2px))',
             },
             colors: {
                 'vlada-color-primary': '#141414',
