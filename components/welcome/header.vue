4c02f944d6c8d946575eec64407d4f16b73925ec
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-09-09 14:25:53 +0400

    Update header.vue

--- components/welcome/header.vue
+++ components/welcome/header.vue
@@ -26,7 +26,7 @@ onBeforeUnmount(() => {
 		<div class="vlada-container">
 			<div class="header__inner">
 				<div class="header__logo">
-					<img src="/img/logo-red.svg" alt="" loading="lazy">
+					<NuxtImg src="/img/logo-red.svg"/>
 				</div>
 				<nav class="header__navigation">
 					<vlada-button type="primary" to="/welcome" text="Home"/>
